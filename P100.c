a6d3ecc4c3f72989e1a0ab5dda6f263dad013b37
Afuness <2587586638@qq.com>
2018-12-20 19:48:29 +0800

    Solve 2 problems.

--- P100.c
+++ P100.c
@@ -1,4 +1,7 @@
-#include "P100.h"
+#include "P005.h"
+#include "P010.h"
+#include <assert.h>
+#include <math.h>
 
 int Multiples_of_3_and_5(int target){
     if(target > 0){
@@ -76,4 +79,65 @@ int Is_palindromic( int num )
 		else return 0;
 	}
 	return 1;
+}
+
+int Smallest_multiple()
+{
+	int a = 2520;
+	for (int i = 1; i <= 20; i++)
+	{
+		if (a%i != 0)
+		{
+			++a;
+			i = 1;
+		}
+	}
+	return a;
+}
+
+int Sum_square_difference()
+{
+	int a=0,b=0;
+	for (int i = 1; i <= 100; ++i)
+	{
+		a+= i*i;
+		b+= i;
+	}
+	b = b*b - a;
+	return b;
+}
+
+int Is_prime(int n)
+{
+	if(n == 1) return 0;
+	if(n < 4) return 1;
+	if(n%2 == 0) return 0;
+	if(n<9) return 1;
+	if(n%3 ==0) return 0;
+	// all prime greater than 6 can be written from 6x+1 or 6x +5.
+	double a = sqrt(n);
+	int f = 5;
+	while (f<=a)
+	{
+	  if(n%f == 0) return 0;
+	  if(n%(f+2) ==0) return 0;
+	  f+=6;
+	}
+	return 1;
+}
+
+int The_10001st_prime()
+{
+    int sum =0;
+	for (int i = 1;; i++)
+	{
+		if (Is_prime( i ))
+		{
+			sum+=1;
+		}
+		if (sum == 10001)
+		{
+			return i;
+		}
+	}
 }
